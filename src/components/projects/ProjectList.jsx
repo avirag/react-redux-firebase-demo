9170381a057b6efb759550a21a7344ff67da6992
Anita Virag <ik.virag@gmail.com>
2019-02-14 17:01:41 +0100

    thunk

--- src/components/projects/ProjectList.jsx
+++ src/components/projects/ProjectList.jsx
@@ -4,7 +4,7 @@ import ProjectSummary from './ProjectSummary';
 const ProjectList = ({projects}) => {
   return (
     <div className="project-list section">
-      { projects && projects.map(project => <ProjectSummary project={project} />) }
+      { projects && projects.map(project => <ProjectSummary key={project.id} project={project} />) }
     </div>
   )
 }

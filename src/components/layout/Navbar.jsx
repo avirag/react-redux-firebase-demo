b70cdede3d35ee49860b70723a17af4a1713e4f5
Anita Virag <ik.virag@gmail.com>
2019-02-15 17:49:22 +0100

    firebase auth with redux

--- src/components/layout/Navbar.jsx
+++ src/components/layout/Navbar.jsx
@@ -2,17 +2,26 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import SignInLinks from './SignedInLinks';
 import SignOutLinks from './SignedOutLinks';
+import { connect } from 'react-redux';
 
-const Navbar = () => {
+const Navbar = props => {
+  const { auth } = props;
+  const links = auth.uid ? <SignInLinks /> : <SignOutLinks />;
+
   return (
     <nav className="nav-wrapper grey darken-3">
       <div className="container">
         <Link to="/" className="brand-logo">MarioPlan</Link>
-        <SignInLinks />
-        <SignOutLinks />
+        { links }
       </div>
     </nav>
   )
 }
 
-export default Navbar;
+const mapStateToProps = state => {
+  return {
+    auth: state.fb.auth
+  }
+}
+
+export default connect(mapStateToProps)(Navbar);
--- src/store/reducers/rootReducer.js
+++ src/store/reducers/rootReducer.js
@@ -3,11 +3,13 @@ import { combineReducers } from 'redux';
 import authReducer from "./authReducer";
 import projectReducer from "./projectReducer";
 import { firestoreReducer } from 'redux-firestore';
+import { firebaseReducer } from 'react-redux-firebase';
 
 const rootReducer = combineReducers({
   auth: authReducer,
   prj: projectReducer,
-  frs: firestoreReducer
+  frs: firestoreReducer,
+  fb: firebaseReducer
 });
 
 export default rootReducer;
